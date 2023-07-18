ca2c3418e6368f459dbd1155a582aef05bdf4783
prasadhonrao <honrao.prasad@gmail.com>
2023-07-18 13:58:23 +0100

    Add logging

--- src/services/order/src/main/java/com/microshop/microservice/order/controllers/OrdersController.java
+++ src/services/order/src/main/java/com/microshop/microservice/order/controllers/OrdersController.java
@@ -6,9 +6,14 @@ import com.microshop.microservice.order.models.OrderRequest;
 import com.microshop.microservice.order.models.OrderResponse;
 import com.microshop.microservice.order.services.OrderService;
 import lombok.RequiredArgsConstructor;
+
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.List;
 
@@ -17,15 +22,18 @@ import java.util.List;
 @RequestMapping("/api/orders")
 public class OrdersController {
 
+    private final Logger logger = LoggerFactory.getLogger(OrdersController.class);
+
     private final OrderService orderService;
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     public ResponseEntity<OrderResponse> placeOrder(@RequestBody OrderRequest orderRequest) {
         try {
+            logger.info("Placing order: {}", orderRequest);
             OrderResponse orderResponse = orderService.createOrder(orderRequest);
             return ResponseEntity.status(HttpStatus.CREATED).body(orderResponse);
         } catch (Exception ex) {
-            System.out.println(ex.getMessage());
+            logger.error("Error placing order", ex);
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
         }
     }
@@ -34,11 +42,13 @@ public class OrdersController {
     @ResponseStatus(HttpStatus.OK)
     public ResponseEntity<List<OrderResponse>> getOrders() {
         try {
+            logger.info("Getting all orders");
             var orders = orderService.getOrders();
             return ResponseEntity.ok(orders);
         } catch (NotFoundException e) {
             return ResponseEntity.notFound().build();
-        } catch (Exception rex) {
+        } catch (Exception ex) {
+            logger.error("Error getting orders",ex);
             return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
@@ -47,11 +57,13 @@ public class OrdersController {
     @ResponseStatus(HttpStatus.OK)
     public ResponseEntity<OrderResponse> getOrder(@PathVariable String orderNumber) {
         try {
+            logger.info("Getting all order details for order {}", orderNumber);
             var order = orderService.getOrder(orderNumber);
             return new ResponseEntity<OrderResponse>(order, HttpStatus.OK);
         } catch (NotFoundException e) {
             return ResponseEntity.notFound().build();
-        } catch (Exception rex) {
+        } catch (Exception ex) {
+            logger.error("Error getting a order",ex);
             return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
