47811d3c345e38fd5514544fd939ac7211a6dd26
prasadhonrao <honrao.prasad@gmail.com>
2023-07-20 16:30:44 +0100

    Remove empty unit test

--- src/services/order/src/test/java/com/microshop/microservice/order/OrderMicroserviceApplicationTests.java
+++ src/services/order/src/test/java/com/microshop/microservice/order/OrderMicroserviceApplicationTests.java
@@ -1,13 +1,8 @@
 package com.microshop.microservice.order;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class OrderMicroserviceApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
 
 }
