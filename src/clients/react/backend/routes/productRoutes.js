d2a8606aa0314663ed56b4c8dd14982ca5be7e40
prasadhonrao <honrao.prasad@gmail.com>
2023-07-22 20:28:19 +0100

    Add productController and integrate it with productRoute

--- src/clients/react/backend/routes/productRoutes.js
+++ src/clients/react/backend/routes/productRoutes.js
@@ -2,25 +2,9 @@ import express from 'express';
 
 const router = express.Router();
 
-import asyncHandler from '../middleware/asyncHandler.js';
-import Product from '../models/productModel.js';
+import {getProducts, getProductById} from '../controllers/productController.js';
 
-router.get('/', asyncHandler(async(req, res) => {
-    const products = await Product.find({});
-    res.send(products);
-}));
-
-router.get('/:id', asyncHandler(async(req, res) => {
-    console.log(req.params.id);
-    const product = await Product.findById(req.params.id);
-
-    if(product) {
-        res.json(product);
-    } else {
-        res.status(404);
-        throw new Error('Product not found');
-    }
-  })
-);
+router.route('/').get(getProducts);
+router.route('/:id').get(getProductById);
 
 export default router;
