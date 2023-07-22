bcdd48b89d19944d5809c8b8ccbdf8834b80874c
prasadhonrao <honrao.prasad@gmail.com>
2023-07-22 20:19:55 +0100

    Added errorHandler middleware

---
+++ src/clients/react/backend/middleware/errorMiddleware.js
@@ -0,0 +1,24 @@
+
+const notFound = (req, res, next) => {
+    const error = new Error(`Not Found - ${req.originalUrl}`);
+    res.status(400);
+    next(error);
+};
+
+const errorHandler = (err, req, res) => {
+    let statusCode = res.statusCode === 200 ? 500 : res.statusCode;
+    let message = err.message;
+    
+    // If the error is a mongoose error, then we want to send a more user friendly message
+    if (err.name === 'CastError' && err.kind === 'ObjectId') {
+        statusCode = 404;
+        message = 'Resource not found';
+    }
+
+    res.status(statusCode).json({ 
+        message, 
+        stack: process.env.NODE_ENV === 'production' ? null : err.stack
+    });
+};
+
+export { notFound, errorHandler };
--- src/clients/react/backend/server.js
+++ src/clients/react/backend/server.js
@@ -4,6 +4,7 @@ import dotenv from 'dotenv';
 import connectDB from './config/db.js';
 
 import productRoutes from './routes/productRoutes.js';
+import { notFound, errorHandler } from './middleware/errorMiddleware.js';
 
 dotenv.config();
 const port = process.env.PORT || 5000;
@@ -17,6 +18,8 @@ app.get('/', (req, res) => {
 });
 
 app.use('/api/products', productRoutes);
+app.use(notFound);
+app.use(errorHandler);
 
 
 app.listen(port, () => {`Server listening on port ${port}!`});
