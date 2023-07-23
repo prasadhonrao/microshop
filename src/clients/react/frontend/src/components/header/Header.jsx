05b35d04031bb545ade1c08eeb924fec3a8c8abe
prasadhonrao <honrao.prasad@gmail.com>
2023-07-23 20:50:39 +0100

    Display order quantity in header

--- src/clients/react/frontend/src/components/header/Header.jsx
+++ src/clients/react/frontend/src/components/header/Header.jsx
@@ -1,35 +1,58 @@
-import { LinkContainer } from 'react-router-bootstrap';
+import { LinkContainer } from "react-router-bootstrap";
 
-import { Navbar, Nav, Container } from 'react-bootstrap';
-import { FaShoppingCart, FaUser } from 'react-icons/fa';
-import logo from '../../assets/logo.png';
+import { Badge, Navbar, Nav, Container } from "react-bootstrap";
+import { FaShoppingCart, FaUser } from "react-icons/fa";
+import { useSelector } from "react-redux";
+import logo from "../../assets/logo.png";
 
 const Header = () => {
-  return (
-    <header>
-        <Navbar bg="dark" variant="dark" expand="lg" collapseOnSelect>
-            <Container>
-                <LinkContainer container="nav" to="/">
-                <Navbar.Brand>
-                    <img src={logo} alt="logo" width="30" height="30" className="d-inline-block align-top" />{' '}
-                    Microshop
-                </Navbar.Brand>
-                </LinkContainer>
-                <Navbar.Toggle aria-controls="basic-navbar-nav" />
-                <Navbar.Collapse id="basic-navbar-nav">
-                    <Nav className="ms-auto">
-                        <LinkContainer container="nav" to="/cart">
-                        <Nav.Link ><FaShoppingCart />Cart</Nav.Link>
-                        </LinkContainer>
-                        <LinkContainer container="nav" to="/signin">
-                        <Nav.Link ><FaUser />Sign In</Nav.Link>
-                        </LinkContainer>
-                    </Nav>
-                </Navbar.Collapse>
-            </Container>
-        </Navbar>
-    </header>
-  )
-}
+  const { cartItems } = useSelector((state) => state.cart);
+  console.log(cartItems);
 
-export default Header
+  return (
+    <>
+      <header>
+        <Navbar bg="dark" variant="dark" expand="lg" collapseOnSelect>
+          <Container>
+            <LinkContainer container="nav" to="/">
+              <Navbar.Brand>
+                <img
+                  src={logo}
+                  alt="logo"
+                  width="30"
+                  height="30"
+                  className="d-inline-block align-top"
+                />{" "}
+                Microshop
+              </Navbar.Brand>
+            </LinkContainer>
+            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+            <Navbar.Collapse id="basic-navbar-nav">
+              <Nav className="ms-auto">
+                <LinkContainer container="nav" to="/cart">
+                  <Nav.Link>
+                    <FaShoppingCart />
+                    Cart
+                  {cartItems.length > 0 && (
+                    <Badge pill bg="success" style={{ marginLeft: "5px" }}>
+                      {cartItems.reduce((a, c) => a + c.qty, 0)}
+                    </Badge>
+                  )}
+                  </Nav.Link>
+                </LinkContainer>
+                <LinkContainer container="nav" to="/signin">
+                  <Nav.Link>
+                    <FaUser />
+                    Sign In
+                  </Nav.Link>
+                </LinkContainer>
+              </Nav>
+            </Navbar.Collapse>
+          </Container>
+        </Navbar>
+      </header>
+    </>
+  );
+};
+
+export default Header;
