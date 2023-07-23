179d12f723d3c8664de9f9a4b8b57add30ddf537
prasadhonrao <honrao.prasad@gmail.com>
2023-07-23 10:42:18 +0100

    Add product detail slice and integrate with ProductPage

--- src/clients/react/frontend/src/pages/product/ProductPage.jsx
+++ src/clients/react/frontend/src/pages/product/ProductPage.jsx
@@ -1,79 +1,93 @@
-import { useEffect, useState } from 'react';
-import {useParams} from 'react-router-dom';
-import { Link } from 'react-router-dom';
-import { Row, Col, Image, ListGroup, Card, Button } from 'react-bootstrap';
+import { useParams } from "react-router-dom";
+import { Link } from "react-router-dom";
+import { Row, Col, Image, ListGroup, Card, Button } from "react-bootstrap";
 
-import Rating from '../../components/rating/Rating';
-import axios from 'axios';
-
+import Rating from "../../components/rating/Rating";
+import { useGetProductDetailsQuery } from "../../slices/productsApiSlice";
 
 const ProductPage = () => {
-    const [product, setProduct] = useState({});
-    const {id: productId} = useParams();
+  const { id: productId } = useParams();
+  const {
+    data: product,
+    error,
+    isLoading,
+  } = useGetProductDetailsQuery(productId);
 
-    useEffect(() => {
-        const fetchProduct = async () => {
-            const {data} = await axios.get(`/api/products/${productId}`);
-            setProduct(data);
-        }
-        fetchProduct();
-    }, [productId]);
-    
   return (
     <>
-        <Link className='btn btn-light my-3' to='/'>
-            Go Back
-        </Link>
-        <Row>
+      <Link className="btn btn-light my-3" to="/">
+        Go Back
+      </Link>
+      {isLoading ? (
+        <h2>Loading...</h2>
+      ) : error ? (
+        <div>{error?.data?.message || error.error}</div>
+      ) : (
+        <>
+          <Row>
             <Col md={5}>
-                <Image src={product.image} alt={product.name} fluid />
+              <Image src={product.image} alt={product.name} fluid />
             </Col>
             <Col md={4}>
-                <ListGroup variant='flush'>
-                    <ListGroup.Item>
-                        <h3>{product.name}</h3>
-                    </ListGroup.Item>
-                    <ListGroup.Item>
-                        <Rating value={product.rating} text={`${product.numReviews} reviews`} />
-                    </ListGroup.Item>
-                    {/* <ListGroup.Item>
+              <ListGroup variant="flush">
+                <ListGroup.Item>
+                  <h3>{product.name}</h3>
+                </ListGroup.Item>
+                <ListGroup.Item>
+                  <Rating
+                    value={product.rating}
+                    text={`${product.numReviews} reviews`}
+                  />
+                </ListGroup.Item>
+                {/* <ListGroup.Item>
                         Price: ${product.price}
                     </ListGroup.Item> */}
-                    <ListGroup.Item>
-                        Description: ${product.description}
-                    </ListGroup.Item>
-                </ListGroup>
+                <ListGroup.Item>
+                  Description: ${product.description}
+                </ListGroup.Item>
+              </ListGroup>
             </Col>
             <Col md={3}>
-                <Card>
-                    <ListGroup variant='flush'>
-                        <ListGroup.Item>
-                            <Row>
-                                <Col>Price:</Col>
-                                <Col>
-                                    <strong>${product.price}</strong>
-                                </Col>
-                            </Row>
-                        </ListGroup.Item>
-                        <ListGroup.Item>
-                            <Row>
-                                <Col>Status:</Col>
-                                <Col>
-                                    <strong>{product.countInStock > 0 ? 'In stock' : 'Out Of Stock'}</strong>
-                                </Col>
-                            </Row>
-                        </ListGroup.Item>
-                        <ListGroup.Item>
-                            <Button className='btn-block' type='button' disabled={product.countInStock === 0}>
-                                Add to Cart 
-                            </Button>
-                        </ListGroup.Item>
-                    </ListGroup>
-                </Card>
+              <Card>
+                <ListGroup variant="flush">
+                  <ListGroup.Item>
+                    <Row>
+                      <Col>Price:</Col>
+                      <Col>
+                        <strong>${product.price}</strong>
+                      </Col>
+                    </Row>
+                  </ListGroup.Item>
+                  <ListGroup.Item>
+                    <Row>
+                      <Col>Status:</Col>
+                      <Col>
+                        <strong>
+                          {product.countInStock > 0
+                            ? "In stock"
+                            : "Out Of Stock"}
+                        </strong>
+                      </Col>
+                    </Row>
+                  </ListGroup.Item>
+                  <ListGroup.Item>
+                    <Button
+                      className="btn-block"
+                      type="button"
+                      disabled={product.countInStock === 0}
+                    >
+                      Add to Cart
+                    </Button>
+                  </ListGroup.Item>
+                </ListGroup>
+              </Card>
             </Col>
-        </Row>
+          </Row>
+        </>
+      )}
+      ;
     </>
-  )
-}
+  );
+};
 
-export default ProductPage
+export default ProductPage;
--- src/clients/react/frontend/src/slices/productsApiSlice.js
+++ src/clients/react/frontend/src/slices/productsApiSlice.js
@@ -9,8 +9,14 @@ export const productsApiSlice = apiSlice.injectEndpoints({
             }),
             keepUnusedDataFor: 5 // keep data for 5 seconds even if it's not used
         }),
+        getProductDetails: builder.query({
+            query: (productId) => ({ 
+                url: `${PRODUCTS_URL}/${productId}`,
+            }),
+            keepUnusedDataFor: 5 // keep data for 5 seconds even if it's not used
+        }),
     })
 });
 
 // Export hooks for usage in functional components and follow naming convention strictly
-export const { useGetProductsQuery } = productsApiSlice;
+export const { useGetProductsQuery, useGetProductDetailsQuery } = productsApiSlice;
