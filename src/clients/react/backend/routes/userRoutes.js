4d517b0696b5b3194bf2576155b7862fd9e27061
prasadhonrao <honrao.prasad@gmail.com>
2023-07-24 12:46:25 +0100

    Add auth middleware and implement logout feature

--- src/clients/react/backend/routes/userRoutes.js
+++ src/clients/react/backend/routes/userRoutes.js
@@ -12,10 +12,12 @@ import { authUser,
     updateUser,
     deleteUser} from '../controllers/userController.js';
 
-router.route('/').post(registerUser).get(getUsers);
+import { protect, admin } from '../middleware/authMiddleware.js';
+
+router.route('/').post(registerUser).get(protect, admin, getUsers);
 router.post('/logout', logoutUser);
-router.post('/login', authUser);
-router.route('/profile').get(getUserProfile).put(updateUserProfile);
-router.route('/:id').get(getUserById).put(updateUser).delete(deleteUser);
+router.post('/auth', authUser);
+router.route('/profile').get(protect, getUserProfile).put(protect, updateUserProfile);
+router.route('/:id').get(protect, admin, getUserById).put(protect, admin, updateUser).delete(protect, admin,deleteUser);
 
 export default router;
