ad34f626f3f5906129590fd85e1466722f85e660
prasadhonrao <honrao.prasad@gmail.com>
2023-07-24 10:07:10 +0100

    Add CartPage

--- src/clients/react/frontend/src/index.js
+++ src/clients/react/frontend/src/index.js
@@ -14,12 +14,14 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import HomePage from './pages/home/HomePage';
 import ProductPage from './pages/product/ProductPage';
+import CartPage from './pages/cart/CartPage';
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" element={<App />}>
       <Route index={true} path="/" element={<HomePage />} />
       <Route path="/product/:id" element={<ProductPage />} />
+      <Route path="/cart" element={<CartPage />} />
       <Route path="about" element={<div>About</div>} /> 
     </Route>
   )
